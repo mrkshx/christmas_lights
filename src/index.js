79fb0f9a084582cafbbe17e5d214f6d3238b9034
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-08 20:46:45 +0100

    changed default colors of bulbs when turned off

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const switcher = document.getElementById("switch");
 const bulbs = document.querySelectorAll(".bulb");
 const body = document.querySelector("body");
-const bulb_color_array = ["yellow", "green", "red", "blue", "yellow", "red", "blue"];
 
 const toggleSwitch = function() {
   if (switcher.textContent.includes("OFF")) {
     switcher.textContent = "Lights ON";
     for (var i=0; i < bulbs.length; i++) {
-      bulbs[i].classList.add(bulb_color_array[i]);
+      bulbs[i].classList.remove("paused");
     }
     body.classList.add("dark-body");
   } else {
     switcher.textContent = "Lights OFF";
     for (var i=0; i < bulbs.length; i++) {
-      bulbs[i].classList.remove(bulb_color_array[i]);
+      bulbs[i].classList.add("paused");
     }
     body.classList.remove("dark-body");
   }
