72aa993a4ff51aac17c1271432afa19528bd5412
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-08 20:26:56 +0100

    body background will change on presing switch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const switcher = document.getElementById("switch");
 const bulbs = document.querySelectorAll(".bulb");
+const body = document.querySelector("body");
 const bulb_color_array = ["yellow", "green", "red", "blue", "yellow", "red", "blue"];
 
 const toggleSwitch = function() {
@@ -8,11 +9,13 @@ const toggleSwitch = function() {
     for (var i=0; i < bulbs.length; i++) {
       bulbs[i].classList.add(bulb_color_array[i]);
     }
+    body.classList.add("dark-body");
   } else {
     switcher.textContent = "Lights OFF";
     for (var i=0; i < bulbs.length; i++) {
       bulbs[i].classList.remove(bulb_color_array[i]);
     }
+    body.classList.remove("dark-body");
   }
 }
 
