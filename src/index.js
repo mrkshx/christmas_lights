d1b95649cbca6d4bc381ee32bf670c3d0faaaa14
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-08 18:34:44 +0100

    switch will now turn on / off the lights

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const switcher = document.getElementById("switch");
+const bulbs = document.querySelectorAll(".bulb");
+const bulb_color_array = ["yellow", "green", "red", "blue", "yellow", "red", "blue"];
 
 const toggleSwitch = function() {
   if (switcher.textContent.includes("OFF")) {
     switcher.textContent = "Lights ON";
+    for (var i=0; i < bulbs.length; i++) {
+      bulbs[i].classList.add(bulb_color_array[i]);
+    }
   } else {
     switcher.textContent = "Lights OFF";
+    for (var i=0; i < bulbs.length; i++) {
+      bulbs[i].classList.remove(bulb_color_array[i]);
+    }
   }
 }
 
