925449386cec5eff8657e3d6889bba82e2ce4f29
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-09 02:19:46 +0100

    added random rotation to bulbs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const switcher = document.getElementById("switch");
 const bulbs = document.querySelectorAll(".bulb");
 const body = document.querySelector("body");
 
+for (var i=0; i < bulbs.length; i++) {
+  bulbs[i].classList.remove("paused");
+  let bulb_rotation = Math.floor(Math.random() * 21) - 20;
+  bulbs[i].style.setProperty("transform", `rotate(${bulb_rotation}deg)`, "important");
+}
+
+
 const toggleSwitch = function() {
   if (switcher.textContent.includes("OFF")) {
     switcher.textContent = "Lights ON";
